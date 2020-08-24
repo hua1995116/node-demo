bbfff8b2e2157ba552f5b99dc240c351b793382d
huayifeng <461249104@qq.com>
2020-08-24 20:32:37 +0800

    feat: file download

---
+++ file-download/server.js
@@ -0,0 +1,124 @@
+const Koa = require('../node-cors/node_modules/koa');
+const router = require('../node-cors/node_modules/koa-router')();
+const koaBody = require('../node-cors/node_modules/koa-body');
+const fs = require('fs');
+const path = require('path');
+const mime = require('../node-cors/node_modules/mime');
+const serve = require('../node-cors/node_modules/koa-static');
+const mount = require('../node-cors/node_modules/koa-mount');
+const app = new Koa();
+
+function getRange(range) {
+    var match = /bytes=([0-9]*)-([0-9]*)/.exec(range);
+    const requestRange = {};
+    if (match) {
+        if (match[1]) requestRange.start = Number(match[1]);
+        if (match[2]) requestRange.end = Number(match[2]);
+    }
+    return requestRange;
+}
+
+app.use(async (ctx, next) => {
+    ctx.set('Access-Control-Max-Age', 600);
+    ctx.set('Access-Control-Allow-Origin', ctx.headers.origin);
+    ctx.set('Access-Control-Allow-Credentials', true)
+    ctx.set('Access-Control-Request-Method', 'PUT,POST,GET,DELETE,OPTIONS')
+    ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, range')
+    if (ctx.method === 'OPTIONS') {
+        ctx.status = 204;
+        return;
+    }
+    await next();
+});
+
+
+app.use(mount('/static', serve('static')));
+
+app.use(mount('/example', serve('example')));
+
+app.use(koaBody({ multipart: true }));
+
+router.get('/api/download', async (ctx) => {
+    const { filename } = ctx.query;
+    const fStats = fs.statSync(path.join(__dirname, './static/', filename));
+    ctx.set({
+        'Content-Type': 'application/octet-stream',
+        'Content-Disposition': `attachment; filename=${filename}`,
+        'Content-Length': fStats.size
+    });
+    ctx.body = fs.readFileSync(path.join(__dirname, './static/', filename));
+})
+
+router.get('/api/downloadUrl', async (ctx) => {
+    const { filename } = ctx.query;
+    ctx.body = {
+        code: 0,
+        data: {
+            url: 'http://localhost:8888/static/1597375650384.jpg',
+        }
+    }
+})
+
+router.head('/api/rangeFile', async (ctx) => {
+    console.log(111);
+    const { filename } = ctx.query;
+    const stats = fs.statSync(path.join(__dirname, './static/', filename));
+    console.log(stats.size);
+    ctx.set('Content-Length', stats.size);
+    ctx.length = stats.size;
+    ctx.status = 200;
+    return;
+    // ctx.body = '';
+})
+
+router.get('/api/rangeFile', async(ctx) => {
+    const { filename } = ctx.query;
+    const { size } = fs.statSync(path.join(__dirname, './static/', filename));
+    const range = ctx.headers['range'];
+    // 3、通知浏览器可以进行分部分请求
+    if (!range) {
+        ctx.set('Accept-Ranges', 'bytes');
+        ctx.body = fs.readFileSync(path.join(__dirname, './static/', filename));
+        return;
+    }
+    const { start, end } = getRange(range);
+    // 4、检查请求范围
+    if (start >= size || end >= size) {
+        ctx.response.status = 416;
+        ctx.set('Content-Range', `bytes */${size}`);
+        ctx.body = '';
+        return;
+    }
+    ctx.response.status = 206;
+    ctx.set('Accept-Ranges', 'bytes');
+    ctx.set('Content-Range', `bytes ${start}-${end ? end : size - 1}/${size}`);
+    ctx.body = fs.createReadStream(path.join(__dirname, './static/', filename), { start, end });
+})
+
+router.get('/api/base64', async (ctx) => {
+    const { filename } = ctx.query;
+    const content = fs.readFileSync(path.join(__dirname, './static/', filename));
+    const fStats = fs.statSync(path.join(__dirname, './static/', filename));
+    console.log(fStats);
+    ctx.body = {
+        code: 0,
+        data: {
+            base64: content.toString('base64'),
+            filename,
+            type: mime.getType(filename)
+        }
+    }
+})
+
+
+router.get('/api/corslist', async (ctx) => {
+    ctx.body = {
+        data: [{name: '秋风的笔记'}]
+    }
+})
+
+
+
+app.use(router.routes());
+
+app.listen(8888);
